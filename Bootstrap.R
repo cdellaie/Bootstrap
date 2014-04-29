9fe83f5247cb86c4cd345981293f8655610293ef
cdellaie <dellaiera.clement@gmail.com>
2014-04-29 22:03:03 +0200

    Code
    Mise à jour : création d'une fonction + application sur deux exemples
    supplémentaires.

--- Bootstrap.R
+++ Bootstrap.R
@@ -1,22 +1,60 @@
-#valeurs extremes
-#premier exemple : loi uniforme
+#Valeurs extremes
 
+#package qui propose une fonction simulant des pareto
+#rpareto(N,location,shape)
+library(VGAM)
+
+# Methode sous echantillonage par MC pour obtenir une estimation de la loi du maximum
+# Tirage sans remise de B echantillons de x=data, bn est la taille des sous-echantillons
+
+BootMC=function(x,B,bn){
+N=length(x);
+Tstar=rep(0,B);
+for (k in 1:B ){
+	i=sample(1:N,bn,replace=FALSE);
+	Tstar[k]=max(x[i]);
+	}
+return(Tstar);
+}
+
+#Premier exemple : loi uniforme
+
+B=100;
 N=100000;
-bn=100;
 theta=8;
 X=runif(N,0,theta);
 
 T=cummax(X);
 S=cummin(X);
 
-#sous-echantillonage : methode de Monte Carlo
-#tirage sans remise de B echantillons
-B=1000;
-Tstar=rep(0,B);
-for (k in 1:B ){
-	i=sample(1:N,bn,replace=FALSE);
-	Tstar[k]=max(X[i]);
-	}
-
+Tstar=BootMC(X,1000,100);
 plot(Tstar[order(Tstar)],(1/B)*(1:B),col='blue',type='s',xlim=c(0,10))
-title(main='FDR empirique du max obtenue par Bootstrap')
+title(main='FDR empirique du max d'une uniforme obtenue par Bootstrap')
+
+
+#loi normale
+B=1000;
+N=100000;
+m=0;
+sigma=1;
+Z=rnorm(N,m,sigma);
+
+Zmax=BootMC(Z,B,100);
+plot(Zmax[order(Zmax)],(1/B)*(1:B),col='blue',type='s')
+title(main='FDR empirique du max d'une normale obtenue par Bootstrap')
+
+#loi de Pareto : queue epaisse
+B=1000;
+N=100000;
+gamma=1;
+alpha=1;
+P=rpareto(N,gamma,alpha);
+
+Pmax=BootMC(P,B,100);
+plot(Pmax[order(Pmax)],(1/B)*(1:B),col='blue',type='s')
+title(main='FDR empirique du max d une pareto obtenue par Bootstrap')
+
+
+
+
+
