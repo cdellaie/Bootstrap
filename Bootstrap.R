8f7e25115b9ee4a9beaed0ccf573e1300d70a4ad
cdellaie <dellaiera.clement@gmail.com>
2014-06-01 16:49:01 +0200

    Mise à jour

--- Bootstrap.R
+++ Bootstrap.R
@@ -1,4 +1,6 @@
-#Valeurs extremes
+		####################
+		# Valeurs extremes #
+		####################
 
 #package qui propose une fonction simulant des pareto
 #rpareto(N,location,shape)
@@ -17,7 +19,9 @@ for (k in 1:B ){
 return(Tstar);
 }
 
-#Premier exemple : loi uniforme
+##################################
+# Premier exemple : loi uniforme #
+##################################
 
 B=100;
 N=100000;
@@ -31,8 +35,10 @@ Tstar=BootMC(X,1000,100);
 plot(Tstar[order(Tstar)],(1/B)*(1:B),col='blue',type='s',xlim=c(0,10))
 title(main='FDR empirique du max d une uniforme obtenue par Bootstrap')
 
+#################
+#  loi normale ##
+#################
 
-#loi normale
 B=1000;
 N=100000;
 m=0;
@@ -43,7 +49,10 @@ Zmax=BootMC(Z,B,100);
 plot(Zmax[order(Zmax)],(1/B)*(1:B),col='blue',type='s')
 title(main='FDR empirique du max d une normale obtenue par Bootstrap')
 
-#loi de Pareto : queue epaisse
+#################################
+# loi de Pareto : queue epaisse #
+#################################
+
 B=1000;
 N=100000;
 gamma=1;
@@ -54,9 +63,11 @@ Pmax=BootMC(P,B,100);
 plot(Pmax[order(Pmax)],(1/B)*(1:B),col='blue',type='s')
 title(main='FDR empirique du max d une pareto obtenue par Bootstrap')
 
-#16/05/2014
-#Pdf de Guillaume
-# Test sur une normale
+#########################
+# 16/05/2014            #
+# Pdf de Guillaume      #
+# Test sur une normale  #
+#########################
 
 e=exp(-1);
 B=1000;
@@ -76,8 +87,10 @@ plot(Ztild,(1/B)*(1:B),col='blue',type='s')
 lines(t,exp(-exp(-t)),col=rgb(0.6,0.3,0.1),type='l');
 title(main='FDR empirique du max d une normale obtenue par Bootstrap')
 
-#01/06/2014
-# estimation du gamma par regression, articles de Bertail
+###########################################################
+# 01/06/2014                                              #
+# estimation du gamma par regression, articles de Bertail #
+###########################################################
 
 Subsample=function(x,bn){
 N=length(x);
@@ -133,11 +146,26 @@ lin<-lm(y ~ log(bn));
 abline(lin, col="red");
 title("Estimation par régression");
 
-# à la main (on trouve un coefficient differrent !)
+# à la main (on trouve un coefficient différent !)
 C=sum( log(bn)- mean(log(bn)) ) ;
 gamma=((sum(y-mean(y)))*C)/C**2;    #0.3
 
+###
+## Section : The extreme value statistics revisited
+## Estimation par difference de quantiles
+###
+t1=0.5;
+t2=0.25;
+for (k in 1:I ){
+	Z=Subsample(X,bn[k]);
+	y[k]=log(abs(quantile(Z,t1)-quantile(Z,t2)));
+	}
 
+plot(y ~ log(bn),pch=4,col="blue"); 
+lin<-lm(y ~ log(bn));
+abline(lin, col="red");
+title("Estimation par régression");
+# intercept : -0.8158 gamma: -0.1108 gamma a la main : -0.3
 
 
 
